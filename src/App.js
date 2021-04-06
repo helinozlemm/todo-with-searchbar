53936b181642a12cb8a2dad4b3288983e33c8591
helinozlemm <helinozlemm96@gmail.com>
2021-04-06 21:22:55 +0300

    fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useEffect } from "react";
 import "./App.css";
 import Form from "./components/Form";
 import ToDoItem from "./components/ToDoItem";
-import { ToDoItems } from "./data";
+import { API_TODO } from "./utils/constans";
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [toDoList, setToDoList] = useState([]);
+
+  useEffect(() => {
+    fetch(API_TODO)
+      .then((response) => response.json())
+      .then((data) => setToDoList(data));
+  }, []);
+
   const filteredToDoItems = useMemo(
     () =>
-      ToDoItems.filter((todo) =>
+      toDoList.filter((todo) =>
         todo.title.toLowerCase().includes(searchTerm.toLowerCase())
       ),
-    [searchTerm]
+    [searchTerm, toDoList]
   );
 
   return (
---
+++ src/utils/constans.js
@@ -0,0 +1,1 @@
+export const API_TODO = "https://606c5a6ec445570017a46dc0.mockapi.io/todos";
